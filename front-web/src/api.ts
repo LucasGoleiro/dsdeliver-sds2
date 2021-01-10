f20f2572c44d3e3cc9c54062f8a7765bba634d81
Lucas Coelho dos Santos <lucas.goleiro1995@gmail.com>
2021-01-09 22:22:12 -0300

    Add env vars

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,8 +1,8 @@
 import { OrderPayload } from './Orders/types';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
-const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAPBOX;
+const API_URL = process.env.REACT_APP_API_URL;
+const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts() {
     return axios(`${API_URL}/products`);
