3477853192d7cb1b8b3464c95e5d71e7de95538e
Lucas Coelho dos Santos <lucas.goleiro1995@gmail.com>
2021-01-12 21:04:01 -0300

    Add orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 
-import Header from './src/Header';
-import Home from './src/Home';
+import Routes from './src/Routes';
 
 export default function App() {
 
@@ -20,9 +19,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Header />
-      <Home />
       <StatusBar style="auto" />
+      <Routes />
     </View>
   );
 }
